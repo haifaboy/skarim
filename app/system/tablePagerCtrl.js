6070ed7af6d2b293d2facf797c0310a47f85b6ea
haifaboy <haifaboy@gmail.com>
2018-12-13 13:51:06 +0200

    fixing login page

--- app/system/tablePagerCtrl.js
+++ app/system/tablePagerCtrl.js
@@ -1,5 +1,8 @@
-app.controller("TablePageCtrl", function($scope, $location, user) {
+app.controller("TablePageCtrl", function($scope, $location, , $routeParams , user) {
       
     if   ( ! user.isLoggedIn() ) {   $location.path("/"); } 
+
+     $scope.ID = $routeParams.ID;  
      
+     alert ($scope.ID )
 });
